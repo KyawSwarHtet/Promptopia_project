438a07f7e34d4d6796e19cc72cecd43ae23ea32a
kyaw swar htet <kyawswarhtet101094@gmail.com>
2023-06-18 22:09:48 +0630

    profile upd

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -7,6 +7,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
 
 const EditPrompt = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
 
@@ -34,25 +35,25 @@ const EditPrompt = () => {
     e.preventDefault();
     setSubmitting(true);
 
-    if (!promptId) return alert("Prompt ID not found")
+    if (!promptId) return alert("Prompt ID not found");
 
-      try {
-        const response = await fetch(`/api/prompt/${promptId}`, {
-          method: "PATCH",
-          body: JSON.stringify({
-            prompt: post.prompt,       
-            tag: post.tag,
-          }),
-        });
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
 
-        if (response.ok) {
-          router.push("/");
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setSubmitting(false);
+      if (response.ok) {
+        router.push("/");
       }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   return (
